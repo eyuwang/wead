0a50e42e3e9e98d97dc8c80a6b338b47ab3a72d4
Eric Wang <eyuwang@gmail.com>
2016-07-09 15:37:25 -0700

    fix error

--- openpub/settings.py
+++ openpub/settings.py
@@ -96,9 +96,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'eyuwang@gmail.com'
-EMAIL_HOST_PASSWORD = 'Jiaerzi0608'
-SERVER_EMAIL = 'eyuwang@gmail.com'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+SERVER_EMAIL = ''
 DEFAULT_FROM_EMAIL = 'ERIC WANG'
 
 # Database
