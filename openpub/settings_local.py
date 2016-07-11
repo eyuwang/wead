4b082b22e26bac0ab1be6c33e4e3b066d4d090bf
Eric Wang <eyuwang@apple.com>
2016-07-11 12:53:03 -0700

    add settings_local.py

--- openpub/settings_local.py
+++ openpub/settings_local.py
@@ -1,1 +1,3 @@
-DJANGO_WYSIWYG_MEDIA_URL = '/Library/Python/2.7/site-packages/ckeditor/static/ckeditor'
+PRODUCTION=False
+if not PRODUCTION:
+    DJANGO_WYSIWYG_MEDIA_URL = '/Library/Python/2.7/site-packages/ckeditor/static/ckeditor'
